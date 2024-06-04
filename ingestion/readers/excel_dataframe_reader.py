1981d3d0e2f93f182cb348117b57fc0a7397604b
Gustavo Miller Santos <gustavo.miller@quintoandar.com.br>
2024-06-03 21:23:22 -0300

    fix dataframe reader warnings

--- ingestion/readers/excel_dataframe_reader.py
+++ ingestion/readers/excel_dataframe_reader.py
@@ -1,6 +1,9 @@
 import pandas as pd
+import warnings
 from readers.dataframe_reader import DataFrameReader
 
 class ExcelDataFrameReader(DataFrameReader):
     def read_single(self, path: str) -> pd.DataFrame:
-        return pd.read_excel(path, index_col=None, header=0)
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore")
+            return pd.read_excel(path, index_col=None, header=0)
