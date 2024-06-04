a9f8792fa32106af8ce2fca596f5897ba1f555c6
Gustavo Miller Santos <gustavo.miller@quintoandar.com.br>
2024-06-03 22:41:51 -0300

    feat: add market value to fact_daily_stock_ownership

--- financial_data/models/dw/fact_daily_stock_ownership.sql
+++ financial_data/models/dw/fact_daily_stock_ownership.sql
@@ -3,11 +3,13 @@
 WITH all_transactions_daily AS (
     SELECT
         t.sk_product,
+        p.product_code AS current_product_code,
         dd.sk_date,
         t.acc_units,
         t.acc_total_cost_brl,
         t.acc_avg_cost_brl,
-        ROW_NUMBER() OVER(PARTITION BY t.sk_product, dd.sk_date ORDER BY t.transaction_nr_for_product DESC) = 1 AS is_last_transaction_of_day
+        ROW_NUMBER() OVER(PARTITION BY t.sk_product, dd.sk_date ORDER BY t.transaction_nr_for_product DESC) = 1 AS is_last_transaction_of_day,
+        dd.date
     FROM
         {{ ref('b3__staging__stock_transactions_with_avg_cost') }} AS t
     JOIN
@@ -22,9 +24,15 @@ SELECT
     sk_date,
     acc_units,
     acc_total_cost_brl,
-    acc_avg_cost_brl
+    acc_avg_cost_brl,
+    close_value_raw_brl * acc_units AS total_market_value_brl,
+    close_value_raw_brl AS market_value_per_unit_brl
 FROM
-    all_transactions_daily
+    all_transactions_daily AS atd
+LEFT JOIN
+    {{ ref('yfinance__staging__daily_stock_close_price') }} AS sp
+        ON atd.current_product_code = sp.ticker
+        AND atd.date = sp.date
 WHERE
     is_last_transaction_of_day
     AND acc_units != 0
